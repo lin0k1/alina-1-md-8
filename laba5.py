50dd39333f9b4184cc6f4f2684affc6115e70053
lin0k1 <alinapopova11042006@gmail.com>
2025-03-26 14:55:37 +0300

    4lab

---
+++ laba5.py
@@ -0,0 +1,46 @@
+# N=int(input("Сколько ты хочешь написать слов?"))
+# a=""
+# st=""
+# i=1
+# while N>=i:
+#     a = input("Напиши любое слово:")
+#     st = st + a + " "
+#     i+=1
+# print(st)
+
+# print("чтоб закончить раньше напиши слово 'stop'")
+# a=""
+# st =""
+# while a!= 'stop':
+#     a = input("Напиши любое слово:")
+#     st = st + a + " "
+# print(st)
+
+# a = input("Напиши любое слово:")
+# for x in a:
+#     if x == 'ф' or x == 'Ф':
+#         print("Ого! Это редкое слово!")
+#         break
+#     if x != 'ф' or x != 'Ф':
+#         print("Эх, это не очень редкое слово...")
+#         break
+
+import random
+a=0
+b=0
+c=0
+i=0
+num=0
+while i<0:
+    a = random.randint(1,10)
+    b= random.randint(1,10)
+    c = random.choice('+' or '-')
+    num
+
+
+
+
+
+
+
+
